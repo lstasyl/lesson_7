a5e971d4b55963e1bc46f262a21cc7d38824a82b
Анастасія <156712861+lstasyl@users.noreply.github.com>
2024-02-06 10:17:42 +0200

    Update say_hi.py

--- say_hi.py
+++ say_hi.py
@@ -1,2 +1,7 @@
-def n():
-    pass
+def say_hi(name, age):
+    text = f'Hi. My name is {name} and I\'m {age} years old'
+    return text
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
